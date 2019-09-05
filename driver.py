a0457818065540a39c3c47caf1ef2c385cff4322
Quan Nguyen <quannguyen@Quans-MacBook-Pro.local>
2019-09-05 10:35:11 -0400

    new files: driver.py, passenger.py, ride.py added

---
+++ driver.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 10:30:08 2019
+
+@author: quannguyen
+"""
+
+class Driver:
+    pass
---
+++ passenger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 10:30:37 2019
+
+@author: quannguyen
+"""
+
+class Passenger:
+    pass
